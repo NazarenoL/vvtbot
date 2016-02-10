5f4238307500e91bbfefadd12b13934a55c4cb9e
Nazareno Lorenzo <nazarenolorenzo@gmail.com>
2016-02-10 04:52:06 -0300

    test canvas autologin

--- server/routes/auth/handlers/facebookAuthCanvasPost.js
+++ server/routes/auth/handlers/facebookAuthCanvasPost.js
@@ -2,7 +2,7 @@
 module.exports = function(server, request) {
 
   	server.passport.authenticate('facebook-canvas', {
-  		successRedirect: '/',
+  		successRedirect: '/test2',
         failureRedirect: '/auth/facebook-canvas/autologin' 
     })(request);
 
