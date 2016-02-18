03befbe7d0eeb03f12b19b8c383e3a3c23888211
Nazareno Lorenzo <nazarenolorenzo@gmail.com>
2016-02-18 20:47:25 -0300

    Test params on canvas login

--- server/routes/auth/handlers/facebookAuthCanvasPost.js
+++ server/routes/auth/handlers/facebookAuthCanvasPost.js
@@ -1,8 +1,13 @@
 
 module.exports = function(server, request) {
 
+	var urlRedirect= server.settins.app.secureUrl + '/';
+	if(typeof request.query.page != undefined && request.query.page =='winners'){
+		urlRedirect = server.settins.app.secureUrl + '/winners?id=' + request.query.id;
+	}
+
   	server.passportfbcanvas.authenticate('facebook-canvas', {
-  		successRedirect: server.settings.app.secureUrl + '/',
+  		successRedirect: urlRedirect,
         failureRedirect: server.settings.app.secureUrl + '/auth/facebook-canvas/autologin' 
     })(request);
 
